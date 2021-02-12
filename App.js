5039efd3e5fdf57208f65fa485cc21d7451737e4
Hampus Olausson <hampus.john.olausson@gmail.com>
2021-02-12 15:51:38 +0100

    fetch api

--- App.js
+++ App.js
@@ -1,22 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
 import axios from 'axios';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
 
 export default function App() {
 
-  const apiuri = "http://www.omdbapi.com/?i=tt3896198&apikey=c50cef57";
+  const apiurl = "http://www.omdbapi.com/?i=tt3896198&apikey=c50cef57";
   const [state, setState] = useState({
     s: "Enter a movie...",
     results: [],
     selected: {}
   });
 
+  const search = () => {
+    axios(apiurl + "&s=" + state.s).then(({ data }) => {
+      let results = data.Search
+      setState(prevState => {
+        return {...prevState, results: results}
+      })
+    })
+  }
 
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Movie DB</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.title}>Movie Time</Text>
+      <TextInput 
+      style={styles.searchbox}
+      onChangeText={text => setState(prevState => {
+        return {...prevState, s: text}
+      })}
+      onSubmitEditing={search}
+      value={state.s}
+      />
     </View>
   );
 }
@@ -28,4 +43,17 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  title: {
+    color: '#ffffff',
+    fontSize: 20
+  },
+  searchbox: {
+    backgroundColor: '#ffffff',
+    borderRadius: 8,
+    fontSize: 20,
+    width: 200,
+    margin: 10,
+    padding: 10,
+
+  }
 });
