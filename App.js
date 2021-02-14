0c90707090bd3e22a675c0b3eca24d6099bf87d6
Hampus Olausson <hampus.john.olausson@gmail.com>
2021-02-14 12:19:59 +0100

    turned into a class

--- App.js
+++ App.js
@@ -1,19 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
-import React, { useState } from 'react';
+import React, { Component, useState } from 'react';
 import axios from 'axios';
 import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import { render } from 'react-dom';
 
-export default function App() {
+class App extends Component {
 
+  render() {
   return (
     <View style={styles.container}>
-      <Text numberOfLines={1} style={styles.title}>Sign in</Text>
+      <Text numberOfLines={1} style={styles.title}>Login</Text>
       <TextInput placeholder="Enter username..." style={styles.input} ></TextInput>
       <TextInput placeholder="Enter password..." style={styles.input} ></TextInput>
-      <Button title="Log In" onPress={() => alert('Registration successful')}></Button>
+      <Button title="Login" onPress={() => alert('Registration successful')}></Button>
+      <Text style={styles.text} >Dont have an account?</Text>
+      <Text style={{color: 'lightblue', textDecoration: 'underline', fontSize: 20 }}
+      onPress={() => Linking.openURL('')}>
+      Sign up
+      </Text>
     </View>
-  );
+  )
+  
 }
+}
+
 
 const styles = StyleSheet.create({
   container: {
@@ -24,15 +34,25 @@ const styles = StyleSheet.create({
   },
   title: {
     color: '#fff',
-    fontSize: 20,
+    fontSize: 30,
     margin: 10
   },
   input: {
     backgroundColor: '#fff',
-    height: 30,
-    margin: 10,
+    height: 40,
+    width: '60%',
+    margin: 15,
     padding: 10,
-    borderRadius: 20
+    borderRadius: 15,
+    outline: 'none'
+  }, 
+  text: {
+    color: '#fff',
+    fontSize: 20,
+    marginTop: 20,
   }
 
 });
+
+
+export default App
