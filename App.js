d53a7d8fb229c6a0320d7f673cb51fd84a8935a3
Hampus Olausson <hampus.john.olausson@gmail.com>
2021-02-14 11:13:06 +0100

    added inputfield

--- App.js
+++ App.js
@@ -1,14 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
 import axios from 'axios';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
 
   return (
     <View style={styles.container}>
-      <Text numberOfLines={1} style={styles.title}>Hello World</Text>
-      <Button title="Register" onPress={() => alert('Registration successful')}></Button>
+      <Text numberOfLines={1} style={styles.title}>Sign in</Text>
+      <TextInput placeholder="Enter username..." style={styles.input} ></TextInput>
+      <TextInput placeholder="Enter password..." style={styles.input} ></TextInput>
+      <Button title="Log In" onPress={() => alert('Registration successful')}></Button>
     </View>
   );
 }
@@ -23,6 +25,14 @@ const styles = StyleSheet.create({
   title: {
     color: '#fff',
     fontSize: 20,
+    margin: 10
+  },
+  input: {
+    backgroundColor: '#fff',
+    height: 30,
+    margin: 10,
+    padding: 10,
+    borderRadius: 20
   }
 
 });
