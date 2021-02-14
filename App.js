3132d497d9bc183cb31de77451c5fb64eeb77456
Hampus Olausson <hampus.john.olausson@gmail.com>
2021-02-14 13:09:17 +0100

    Started a todo

--- App.js
+++ App.js
@@ -1,58 +1,42 @@
 import { StatusBar } from 'expo-status-bar';
-import React, { Component, useState } from 'react';
-import axios from 'axios';
-import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
-import { render } from 'react-dom';
+import React, { useState } from 'react';
+import { StyleSheet, View, FlatList } from 'react-native';
 
-class App extends Component {
+export default function App() {
 
-  render() {
+    const [todos, setTodos] = useState([
+      {text: 'go to the gym', key: '1'},
+      {text: 'But groceries', key: '2'},
+      {text: 'call the insurance company', key: '3'}
+    ]);
+
   return (
     <View style={styles.container}>
-      <Text numberOfLines={1} style={styles.title}>Login</Text>
-      <TextInput placeholder="Enter username..." style={styles.input} ></TextInput>
-      <TextInput placeholder="Enter password..." style={styles.input} ></TextInput>
-      <Button title="Login" onPress={() => alert('Registration successful')}></Button>
-      <Text style={styles.text} >Dont have an account?</Text>
-      <Text style={{color: 'lightblue', textDecoration: 'underline', fontSize: 20 }}
-      onPress={() => Linking.openURL('')}>
-      Sign up
-      </Text>
+      <View style={styles.content}>
+        <View style={styles.list}>
+          <FlatList
+            data={todos}
+            renderItem={({ item }) => (
+              <Text>{item.text}</Text>
+            )}
+          />
+        </View>
+      </View>
     </View>
-  )
+  );
   
 }
-}
-
 
 const styles = StyleSheet.create({
   container: {
     flex: 1, 
-    backgroundColor: '#223343',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    color: '#fff',
-    fontSize: 30,
-    margin: 10
-  },
-  input: {
     backgroundColor: '#fff',
-    height: 40,
-    width: '60%',
-    margin: 15,
-    padding: 10,
-    borderRadius: 15,
-    outline: 'none'
-  }, 
-  text: {
-    color: '#fff',
-    fontSize: 20,
-    marginTop: 20,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  content: {
+    padding: 40,
   }
 
 });
 
-
-export default App
